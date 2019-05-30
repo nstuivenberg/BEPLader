edc66e4459c2690af72a8e20e51ea2686d5c9fab
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-05-30 15:25:14 +0200

    nullpointer why: test zonder beforeeach

--- src/test/java/nl/hu/bep/group4/bifi/lader/MongoLaderImplTest.java
+++ src/test/java/nl/hu/bep/group4/bifi/lader/MongoLaderImplTest.java
@@ -3,16 +3,14 @@ package nl.hu.bep.group4.bifi.lader;
 import nl.hu.bep.group4.bifi.exceptions.GarbageDataException;
 import nl.hu.bep.group4.bifi.lader.implementations.MongoLaderImpl;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MongoLaderImplTest {
 
 	MongoLaderImpl mongo = null;
-	
-	@BeforeEach
-	public void BeforeEach() {
-		mongo = new MongoLaderImpl();
+
+	public MongoLader setup() {
+		return new MongoLaderImpl();
 	}
 	
 	@Test
