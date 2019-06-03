1d580bd731c75658d191e547d28445aa6731abbd
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-06-03 10:51:06 +0200

    Refactoring of: AdresLaderImpl. Factuuradres wordt nu wel opgehaald De rest komt nog.

--- src/main/java/nl/hu/bep/group4/bifi/lader/AdresLader.java
+++ src/main/java/nl/hu/bep/group4/bifi/lader/AdresLader.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
+import nl.hu.bep.group4.bifi.exceptions.GarbageDataException;
 import nl.hu.bep.group4.bifi.model.Adres;
 
 public interface AdresLader {
 	List<Adres> getAdressen(int klantId) throws SQLException, IOException, ClassNotFoundException;
-	Adres getFactuurAdres(int klantId);
+	Adres getFactuurAdres(int klantId) throws GarbageDataException, SQLException, ClassNotFoundException, IOException;
 }
--- src/main/java/nl/hu/bep/group4/bifi/lader/implementations/MysqlLaderImpl.java
+++ src/main/java/nl/hu/bep/group4/bifi/lader/implementations/MysqlLaderImpl.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class MysqlLaderImpl implements MysqlLader {
 
-    private static final String ADRESSTYPE = "F";
+    private static final String ADRESFACTUURTYPE = "F";
 
     private Connection con = null;
 
@@ -31,8 +31,8 @@ public class MysqlLaderImpl implements MysqlLader {
     public List<Adres> getAdressen(int klantId) throws SQLException, ClassNotFoundException {
         connectDatabase();
         Statement stmt;
-        ResultSet resultSet = null;
-        String query = "select * from Adres where KlantID = " + klantId + " AND Type <> '" + ADRESSTYPE + "'";
+        ResultSet resultSet;
+        String query = "select * from Adres where KlantID = " + klantId + " AND Type <> '" + ADRESFACTUURTYPE + "'";
         List<Adres> adressen = new ArrayList<>();
 
         stmt = con.createStatement();
@@ -120,7 +120,7 @@ public class MysqlLaderImpl implements MysqlLader {
         Statement stmt;
         ResultSet resultSet = null;
 
-        String query = "select * from Adres where KlantID = " + klantId + "AND type = " + ADRESSTYPE;
+        String query = "select * from Adres where KlantID = " + klantId + "AND type = " + ADRESFACTUURTYPE;
 
         Adres factuurAdres = null;
 
@@ -158,7 +158,7 @@ public class MysqlLaderImpl implements MysqlLader {
             throw new GarbageDataException("Meer dan 1 Persoon voor PersoonId" + persoonId);
         }
         while (resultSet.next()) {
-            Integer persoonsId = resultSet.getInt("PersoonID");
+            int persoonsId = resultSet.getInt("PersoonID");
             String voornaam = resultSet.getString("Voornaam");
             String tussenvoegsel = resultSet.getString("Tussenvoegsel");
             String achternaam = resultSet.getString("Achternaam");
